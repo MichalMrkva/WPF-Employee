124502e91f37daa070b5a3c053f047d835deec67
MichalMrkva <michal.mrkva1248@gmail.com>
2023-01-23 15:17:13 +0100

    2

--- WPF Employee/MainWindow.xaml.cs
+++ WPF Employee/MainWindow.xaml.cs
@@ -5,20 +5,23 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Threading;
+using System.Text.RegularExpressions;
+using System.Windows.Media;
 
 namespace WpfBinding
 {
     public partial class MainWindow : Window
     {
+        
 
         Employee em = new Employee()
         {
             FName = "",
             SName = "",
-            BDay = "",
-            HGrad = "",
+            BDay = "01.01.2000",
+            HGrad = "Základní škola",
             Job = "",
-            Salary = 0
+            Salary = ""
 
         };
         public MainWindow()
@@ -30,7 +33,7 @@ namespace WpfBinding
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
 
-            if (em.FName != "" && em.SName != "" && em.BDay != "" && em.HGrad != "" && em.Job != "" && em.Salary > 0)
+            if (em.FName != "" && em.SName != "" && em.BDay != "" && em.HGrad != "" && em.Job != "" && Double.Parse(em.Salary) > 1000)
             {
 
                 string s = "";
@@ -46,7 +49,7 @@ namespace WpfBinding
                 using (StreamWriter sw = new StreamWriter("Employees.txt"))
                 {
                     sw.Write(s);
-                    sw.WriteLine($"Člověk číslo: {i / 2 + 1}");
+                    sw.Write($"Člověk číslo: {i / 2 + 1} ");
                     sw.Write("Jméno: " + em.FName + " ");
                     sw.Write("Příjmení: " + em.SName + " ");
                     sw.Write("Narozeniny: " + em.BDay + " ");
@@ -60,7 +63,7 @@ namespace WpfBinding
                 em.BDay = "";
                 em.HGrad = "";
                 em.Job = "";
-                em.Salary = 0;
+                em.Salary = "";
             }
             else
             {
@@ -68,17 +71,29 @@ namespace WpfBinding
             }
         }
     }
-    class Person : INotifyPropertyChanged
+    class Person : MainWindow, INotifyPropertyChanged
     {
+        Regex str = new Regex(@"^[a-z]{3,20}$");
+        Regex bday = new Regex(@"(?<day>0[1-9]|1[0-9]|2[0-9]|3[01]).(?<month> 0[1 - 9] | 1[012]).(?<year>[0 - 9]{4})");
+        Regex salary = new Regex(@"^\d{5,6}$");
         private string fName;
         public string FName
         {
             get => fName;
             set
             {
-                fName = value;
+                if (str.IsMatch(value))
+                {
+                    fName = value;
+                    //tbFName.Background = ;
+                    //labErrorFirstName.Background = "";
+                }
+                else
+                {
+
+                }
                 OnPropertyChanged("FName");
-                OnPropertyChanged("Status");
+                
             }
         }
         private string sName;
@@ -89,7 +104,7 @@ namespace WpfBinding
             {
                 sName = value;
                 OnPropertyChanged("SName");
-                OnPropertyChanged("Status");
+                
             }
         }
         private string bDay;
@@ -100,7 +115,7 @@ namespace WpfBinding
             {
                 bDay = value;
                 OnPropertyChanged("BDay");
-                OnPropertyChanged("Status");
+                
             }
         }
 
@@ -121,7 +136,7 @@ namespace WpfBinding
             {
                 hGrad = value;
                 OnPropertyChanged("HGrad");
-                OnPropertyChanged("Status");
+                
             }
         }
         private string job;
@@ -132,18 +147,18 @@ namespace WpfBinding
             {
                 job = value;
                 OnPropertyChanged("Job");
-                OnPropertyChanged("Status");
+                
             }
         }
-        private int salary;
-        public int Salary
+        private string salary;
+        public string Salary
         {
             get => salary;
             set
             {
                 salary = value;
                 OnPropertyChanged("Salary");
-                OnPropertyChanged("Status");
+                
             }
         }
 
